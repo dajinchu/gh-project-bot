5eadb1004a70b7f35a8db28a5667bcf4e52e28ab
dajinchu <dajinchu@gmail.com>
2020-04-29 04:12:16 -0400

    add to project based on label

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.labeled', async (context) => {
-    console.log(context)
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  app.on('issues.labeled', async ({ payload, github }) => {
+    const projects = (await github.projects.listForRepo({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name
+    })).data
+    if (projects.length === 0) {
+      console.error('could not find a project board on this repo')
+      return;
+    }
+    const projectId = projects[0].id;
+
+    // Get label(s) that start with status/
+    const labels = payload.issue.labels.filter(l => l.name.startsWith('status/'));
+    if (labels.length === 0) {
+      // no status label
+      return;
+    }
+    const status = labels[0].name.substring(7)
+
+    // Find the corresponding column
+    const columns = await github.projects.listColumns({ project_id: projectId });
+    const destColumn = columns.data.find(c => c.name === status);
+    if (destColumn === undefined) {
+      console.error('could not find corresponding column on project board')
+      return;
+    }
+    
+    await github.projects.createCard({
+      column_id: destColumn.id,
+      content_id: payload.issue.id,
+      content_type: 'Issue'
+    })
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
