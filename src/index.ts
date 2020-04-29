c0f6ef7b56032097838284470e2e4becd52a154f
dajinchu <dajinchu@gmail.com>
2020-04-29 11:02:44 -0400

    tests and husky prettier

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,56 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from "probot"; // eslint-disable-line no-unused-vars
+
+const ISSUE_TO_COLUMN: Record<string, string> = {
+  "status/triage": "Triage",
+  "status/ready-to-work-on": "Ready to Work On",
+  "status/assigned": "Assigned",
+  "status/in-progress": "In Progress",
+  "status/done": "Done",
+};
 
 export = (app: Application) => {
-  app.on('issues.labeled', async ({ payload, github }) => {
-    const projects = (await github.projects.listForRepo({
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name
-    })).data
+  app.on("issues.labeled", async ({ payload, github }) => {
+    const projects = (
+      await github.projects.listForRepo({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+      })
+    ).data;
     if (projects.length === 0) {
-      console.error('could not find a project board on this repo')
+      console.error("could not find a project board on this repo");
       return;
     }
     const projectId = projects[0].id;
 
     // Get label(s) that start with status/
-    const labels = payload.issue.labels.filter(l => l.name.startsWith('status/'));
+    const labels: string[] = payload.issue.labels
+      .map((l) => l.name)
+      .filter((n) => n in ISSUE_TO_COLUMN);
     if (labels.length === 0) {
       // no status label
       return;
     }
-    const status = labels[0].name.substring(7)
+    const columnName = ISSUE_TO_COLUMN[labels[0]];
 
     // Find the corresponding column
-    const columns = await github.projects.listColumns({ project_id: projectId });
-    const destColumn = columns.data.find(c => c.name === status);
+    const columns = await github.projects.listColumns({
+      project_id: projectId,
+    });
+    const destColumn = columns.data.find((c) => c.name === columnName);
     if (destColumn === undefined) {
-      console.error('could not find corresponding column on project board')
+      console.error("could not find corresponding column on project board");
       return;
     }
-    
+
     await github.projects.createCard({
       column_id: destColumn.id,
       content_id: payload.issue.id,
-      content_type: 'Issue'
-    })
-  })
+      content_type: "Issue",
+    });
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,58 +1,102 @@
 // You can import your modules
 // import index from '../src/index'
 
-import nock from 'nock'
+import nock from "nock";
 // Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
-// Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
-const fs = require('fs')
-const path = require('path')
+import myProbotApp from "../src";
+import { Probot } from "probot";
+import fs from "fs";
+import path from "path";
 
-describe('My Probot app', () => {
-  let probot: any
-  let mockCert: string
+const nockGH = nock("https://api.github.com");
+
+const REPO = {
+  name: "gh-project-bot",
+  owner: {
+    login: "dajinchu",
+  },
+};
+
+describe("My Probot app", () => {
+  let probot: any;
+  let mockCert: string;
 
   beforeAll((done: Function) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err: Error, cert: string) => {
-      if (err) return done(err)
-      mockCert = cert
-      done()
-    })
-  })
+    fs.readFile(
+      path.join(__dirname, "fixtures/mock-cert.pem"),
+      "utf8",
+      (err: Error | null, cert: string) => {
+        if (err) return done(err);
+        mockCert = cert;
+        done();
+      }
+    );
+  });
 
   beforeEach(() => {
-    nock.disableNetConnect()
-    probot = new Probot({ id: 123, cert: mockCert })
+    nock.disableNetConnect();
     // Load our app into probot
-    probot.load(myProbotApp)
-  })
+    probot = new Probot({ id: 123, cert: mockCert });
+    probot.load(myProbotApp);
+  });
 
-  test('creates a comment when an issue is opened', async (done) => {
+  test("moves issue to project board when tagged", async (done) => {
     // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+    nockGH
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" });
 
-    // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
-        return true
+    nockGH
+      .get("/repos/dajinchu/gh-project-bot/projects")
+      .reply(200, [{ id: 42 }]);
+
+    nockGH.get("/projects/42/columns").reply(200, [
+      { id: 1, name: "Assigned" },
+      { id: 3, name: "In Review" },
+    ]);
+
+    // Test that card created in assigned column
+    nockGH
+      .post("/projects/columns/1/cards", (body: any) => {
+        done(
+          expect(body).toMatchObject({
+            content_id: 123456,
+            content_type: "Issue",
+          })
+        );
+        return true;
       })
-      .reply(200)
+      .reply(201);
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
-  })
+    await probot.receive({
+      name: "issues",
+      payload: {
+        action: "labeled",
+        issue: {
+          id: 123456,
+          user: {
+            login: "dajinchu",
+          },
+          labels: [
+            {
+              name: "priority/high",
+            },
+            {
+              name: "status/assigned",
+            },
+          ],
+        },
+        repository: REPO,
+      },
+    });
+  });
 
   afterEach(() => {
-    nock.cleanAll()
-    nock.enableNetConnect()
-  })
-})
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
